d1025361c52d5e218c0516dc21e95231a124161b
cipepser <respepic@gmail.com>
2018-12-08 21:42:22 +0900

    get time of utc

---
+++ README.md
@@ -0,0 +1,7 @@
+# rust-time
+
+[Rustで日時を扱う](https://qiita.com/fujitayy/items/ae6175118cbed7134594)
+を自分でも触ってイメージを掴んでおく。
+
+## References
+* [Rustで日時を扱う](https://qiita.com/fujitayy/items/ae6175118cbed7134594)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate chrono;
+
+use chrono::{Utc, Local, DateTime, Date};
+
 fn main() {
-    println!("Hello, world!");
+    let utc_datetime: DateTime<Utc> = Utc::now();
+    let utc_date: Date<Utc> = Utc::today();
+
+    println!("{}", utc_datetime);
+    println!("{}", utc_date);
 }
+
